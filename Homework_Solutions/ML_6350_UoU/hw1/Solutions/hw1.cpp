661b98184e3658130c3eb27a43caf6390b5331b2
vinaykumar2491 <vinaykumar2491@gmail.com>
2017-12-06 03:28:50 +0530

    hw1Sol_v5: generateFeatureVector() update

--- Homework_Solutions/ML_6350_UoU/hw1/Solutions/hw1.cpp
+++ Homework_Solutions/ML_6350_UoU/hw1/Solutions/hw1.cpp
@@ -8,11 +8,7 @@ void f_parseData(std::string& singleInstance, type_vvs& dataMatrix, int rowIndex
 	// parsing the training and test data as Vector<Vector>
 	int attr_count = 0;
 	type_vs tempStorage;				// storing the first/middle/last names
-	type_vi tempIndex;					// storing the indices of the delim in each substrings
 	std::string my_substring;
-	// auto itr = tempStorage.begin();
-	int x =1;
-	// std::string tempSubsubstring;
 	
 	std::size_t delimIndex = singleInstance.find_first_of(attrDelim);
 	my_substring = singleInstance.substr(delimIndex+1, std::string::npos);
@@ -22,7 +18,6 @@ void f_parseData(std::string& singleInstance, type_vvs& dataMatrix, int rowIndex
 		std::cout << delimIndex << ", " ;
 		// std::cout << "[" << my_substring << "] ";
 		delimIndex = my_substring.find_first_of(attrDelim);
-		// tempSubsubstring = my_substring.substr(0, delimIndex);
 		tempStorage.push_back(my_substring.substr(0, delimIndex));
 		my_substring = my_substring.substr(delimIndex+1, std::string::npos);
 		std::cout << tempStorage[attr_count-1] << ", "  << std::endl;
@@ -48,11 +43,13 @@ void f_parseData(std::string& singleInstance, type_vvs& dataMatrix, int rowIndex
 			break;
 		default:	// for (attr_count > 3)
 			tempStorage.insert(tempStorage.begin(), singleInstance.substr(0,1));
+			
+			// creating the Full MiddleName
 			for(int i = 3; i < tempStorage.size()-1; i++){
-				// tempStorage[tempStorage.begin()+1] += " ";
 				tempStorage[2] += " " + tempStorage[i];
 				i++;
 			}
+
 			tempStorage.erase(tempStorage.begin()+3, tempStorage.end()-1);
 			dataMatrix.push_back(tempStorage);
 			break;
@@ -66,18 +63,23 @@ void f_parseData(std::string& singleInstance, type_vvs& dataMatrix, int rowIndex
 							<< dataMatrix[rowIndex][1] << ", "
 							<< dataMatrix[rowIndex][2] << ", "
 							<< dataMatrix[rowIndex][3] << "\n" << std::endl;
-							// << dataMatrix[rowIndex][2] << std::endl;
 
-	// filling the data Matrix with the attributes
+}
+
+void f_generateFeatureVector(vvs& dataMatrix){
+
 	/*
-	 * <feature vector> :  <(firstName > lastName | Y/N), 
+	 * <feature vector> :  <
+	 *						(firstName > lastName | Y/N), 
 	 *						(middleName == EMPTY | Y/N),
 	 *						(firstName[0] == firstName[firstName.length()-1] | Y/N),
 	 *						(firstName > lastName | Y/N),
 	 *						(firstName[1] == {a,e,i,o,u} | Y/N),
 	 *						(lastName.length() % 2 == 0 | Y/N)>
 	 */
-	
+
+
+
 }
 
 
@@ -87,35 +89,33 @@ int main(int argc, char const *argv[])
 	// parsing the training and test data as Vector<Vector>
 	std::string my_attrDelim = " ";
 	std::string singleDataInstance;
-	std::ifstream trainDataFile;
-	std::ifstream testDataFile;
+	std::ifstream trainFile;
+	std::ifstream testFile;
 	type_vvs trainDataTable;
 	type_vvs testDataTable;
 	int noOfTrainSamples = 0;
 	int noOfTestSamples = 0;
 
-	// take the 1st argument while running the executable and parse the train/test data stored in it
-	
 	// pruning training Data
-	trainDataFile.open(argv[1]);
-	if(!trainDataFile){
+	trainFile.open(argv[1]);
+	if(!trainFile){
 		std::cerr << "ERROR: The input training data file is Empty!! Exiting the code." << std::endl;
 		return(-1);
 	}
-	while(std::getline(trainDataFile, singleDataInstance)){
+	while(std::getline(trainFile, singleDataInstance)){
 		std::cout << singleDataInstance << std::endl;
 		f_parseData(singleDataInstance, trainDataTable, noOfTrainSamples, my_attrDelim);
 		noOfTrainSamples++;
 	}
-	trainDataFile.close();
+	trainFile.close();
 
 	// pruning test data
-	testDataFile.open(argv[2]);
-	if(!testDataFile){
+	testFile.open(argv[2]);
+	if(!testFile){
 		std::cerr << "WARINING: The 'test data' is empty!!! Exiting the code." << std::endl;
 		return(0);		// successfully built the tree but failed to evaluate on NULL test data
 	}
-	while(std::getline(testDataFile, singleDataInstance)){
+	while(std::getline(testFile, singleDataInstance)){
 		std::cout << singleDataInstance << std::endl;
 		f_parseData(singleDataInstance, testDataTable, noOfTestSamples, my_attrDelim);
 		noOfTestSamples++;
@@ -128,6 +128,9 @@ int main(int argc, char const *argv[])
 	std::cout << noOfTrainSamples << "," << noOfTestSamples << std::endl;
 	// // std::cout << 
 
+	// Generating the feature vector for each instance training and test data
+	f_generateFeatureVector(trainDataTable);
+	f_generateFeatureVector(testDataTable);
 
 
 }
